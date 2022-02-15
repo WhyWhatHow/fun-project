02f2b3fa6e1ef46fc5e3fce2fc2294371d3d15b0
whywhathow <whywhathow.fun@gmail.com>
2022-02-15 23:04:24 +0800

    _02 . spring.profile.active='@profile.active@'  解决: 利用spring.profile + maven.profile + idea 实现生产环境 一键切换,不需要重复修改 application.yml文件
    遇到的问题: idea可能不生效, 不生效一定要及时重启idea
    
    学到的东西: ctrl+shift+F9 rebuild project/moudle

--- fun-services/fun-service-demo/src/main/java/io/github/whywhathow/controller/DemoController.java
+++ fun-services/fun-service-demo/src/main/java/io/github/whywhathow/controller/DemoController.java
@@ -1,5 +1,6 @@
 package io.github.whywhathow.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,13 @@ public class DemoController {
     @GetMapping("/test")
     public String test(String name) {
         return " hello, fun_project" + name;
+    }
+
+    @Value("${com.name}")
+    String name;
+
+    @GetMapping("/mult/env")
+    public String testMultEnv() {
+        return " now in " + name;
     }
 }
