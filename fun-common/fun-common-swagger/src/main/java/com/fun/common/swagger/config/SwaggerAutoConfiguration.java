8dce3b8af152cdaf7f3f1b4a211b403a31830c2c
whywhathow <whywhathow.fun@gmail.com>
2022-02-21 23:26:25 +0800

    refactor(_10 swagger初步添加):

---
+++ fun-common/fun-common-swagger/src/main/java/com/fun/common/swagger/config/SwaggerAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.fun.common.swagger.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @program: fun-project
+ * @description: swagger自动装配类
+ * @author: WhyWhatHow
+ * @create: 2022-02-21 13:57
+ **/
+@EnableSwagger2
+@Configuration
+public class SwaggerAutoConfiguration {
+
+
+}
