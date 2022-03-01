14433c88ff1487e5a2ad5e564398551171eba535
whywhathow <whywhathow.fun@gmail.com>
2022-03-01 22:36:38 +0800

    feat(nacos +gateway 静态路由)

---
+++ fun-gateway/src/main/java/com/fun/gateway/FunGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.fun.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: fun-project
+ * @description: gateway 网关启动类 ,默认端口9000
+ * @author: WhyWhatHow
+ * @create: 2022-03-01 10:58
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FunGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunGatewayApplication.class, args);
+    }
+}
