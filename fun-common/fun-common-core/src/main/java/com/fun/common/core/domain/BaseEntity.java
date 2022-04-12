6da07e5c70b4875f80b869b48ca71d5e6cc42cef
whywhathow <whywhathow.fun@gmail.com>
2022-04-12 08:20:45 +0800

    fix(解决druid配置导致的逻辑删除问题):
    delete -> update
    reason:
    druid 配置可以拦截删除操作.
    mbplus: 默认配置执行删除操作, 带来错误

--- fun-common/fun-common-core/src/main/java/com/fun/common/core/domain/BaseEntity.java
+++ fun-common/fun-common-core/src/main/java/com/fun/common/core/domain/BaseEntity.java
@@ -2,6 +2,8 @@ package com.fun.common.core.domain;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -21,6 +23,7 @@ public class BaseEntity implements Serializable {
 //    @DateTimeFormat(pattern = "yyyy-MM-dd")
 //    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
     @TableField(fill = FieldFill.INSERT)
+    @Schema(description = "创建时间")
     private Date createTime;
     /**
      * 更新时间, insert and update 时自动更新
@@ -28,13 +31,17 @@ public class BaseEntity implements Serializable {
 //    @DateTimeFormat(pattern = "yyyy-MM-dd")
 //    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Schema(description = "更新时间")
     private Date updateTime ;
     /**
      * 状态位
      */
+    @Schema(description = "状态位")
     private Integer status = 0;
     /**
      * 删除标记, 0, 表示未删除, 1==删除
      */
+    @TableLogic
+    @Schema(description = "删除标记位")
     private Integer delFlag = 0;
 }
